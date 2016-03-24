b2085876411aee76032317dccd0a74f6ac9b139e
yemengqi <738741554@qq.com>
2016-03-24 09:48:14 +0800

    WaterEffect

--- Assets/Scripts/WaterEffect/WaterEffect.cs
+++ Assets/Scripts/WaterEffect/WaterEffect.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 
 public class WaterEffect : MonoBehaviour
 {
-
 	public int WaveWidth = 128;
 	public int WaveHeight = 128;
+	public int Radius = 20;
+	public float Threshold = 0.1f;
 
 	private float[,] waveA;
 	private float[,] waveB;
@@ -18,34 +20,51 @@ public class WaterEffect : MonoBehaviour
 		waveA = new float[WaveWidth, WaveHeight];
 		waveB = new float[WaveWidth, WaveHeight];
 		tex_uv = new Texture2D(WaveWidth, WaveWidth);
-		Puppop();
+		GetComponent<Renderer>().material.SetTexture("_WaveTex", tex_uv);
 	}
 
-	private void Puppop()
+	private void PupDrop(int x, int y)
 	{
-		int w = WaveWidth / 2;
-		int h = WaveHeight / 2;
-		waveA[w, h] = 1;
-		waveA[w + 1, h + 1] = 1;
-		waveA[w + 1, h] = 1;
-		waveA[w + 1, h - 1] = 1;
-		waveA[w, h - 1] = 1;
-		waveA[w - 1, h - 1] = 1;
-		waveA[w - 1, h] = 1;
-		waveA[w - 1, h + 1] = 1;
+		float dist;
+
+		for (int i = -Radius; i <= Radius; i++)
+		{
+			for (int j = -Radius; j <= Radius; j++)
+			{
+				if (((x + i >= 0) && (x + i < WaveWidth - 1)) && ((y + j >= 0) && (y + j < WaveHeight - 1)))
+				{
+					dist = Mathf.Sqrt(i * i + j * j);
+					if (dist < Radius)
+						waveA[x + i, y + j] = Mathf.Cos(dist * Mathf.PI / Radius);
+				}
+			}
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				Vector3 pos = hit.point;
+				Vector3 local = transform.worldToLocalMatrix.MultiplyPoint(pos);
+				int w = (int)((local.x + 0.5) * WaveWidth);
+				int h = (int)((local.y + 0.5) * WaveHeight);
+				PupDrop(w, h);
+			}
+		}
 		ComputeWave();
 	}
 
 	private void ComputeWave()
 	{
-		for (int w = 0; w < WaveWidth; w++)
+		for (int w = 1; w < WaveWidth - 1; w++)
 		{
-			for (int h = 0; h < WaveHeight; h++)
+			for (int h = 1; h < WaveHeight - 1; h++)
 			{
 				waveB[w, h] =
 					(
@@ -70,6 +89,11 @@ public class WaterEffect : MonoBehaviour
 				tex_uv.SetPixel(w, h, new Color(r, g, 0));
 
 				waveB[w, h] -= waveB[w, h] * 0.01f;
+
+				if (Mathf.Abs(waveB[w, h]) < Threshold)
+				{
+					waveB[w, h] = 0;
+				}
 			}
 		}
 		tex_uv.Apply();
---
+++ Assets/Shader_CG/WaterEffect.shader
@@ -0,0 +1,58 @@
+﻿Shader "Unlit/WaterEffect"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			sampler2D _WaveTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float2 uv = tex2D(_WaveTex,i.uv).xy;
+				uv = (uv-0.5)*2;
+				i.uv += uv;
+				fixed4 col = tex2D(_MainTex, i.uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
